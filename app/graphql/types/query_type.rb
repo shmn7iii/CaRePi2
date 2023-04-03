9e61b89b14b7b30d21c184a24d328e08cee7af0c
Shimamura <46078198+shmn7iii@users.noreply.github.com>
2023-04-03 23:40:32 +0900

    add: create UserQuery

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,5 +2,13 @@ module Types
   class QueryType < Types::BaseObject
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
+
+    field :user, UserType, null: true do
+      argument :username, String, required: true
+    end
+
+    def user(username:)
+      User.find_by(username:)
+    end
   end
 end
