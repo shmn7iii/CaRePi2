d4b4f92f5a500aa68bd285941c2e2719d6dc8404
Shimamura <46078198+shmn7iii@users.noreply.github.com>
2023-04-16 11:39:23 +0900

    update: define root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
 
+  root to: 'slack/oauth#invite'
+
   namespace :slack do
     get '/oauth/invite', to: 'oauth#invite'
-    get '/oauth/callback', to: 'oauth#callback'
-    get '/oauth/success', to: 'oauth#success'
-
     post '/events/callback', to: 'events#callback'
+    get '/oauth/success', to: 'oauth#success'
   end
 
   namespace :admin do
+    root to: 'users#index'
+
     resources :users, only: %i[index show edit update destroy]
     resources :entrance_logs, only: %i[index show edit update destroy]
-
-    root to: 'users#index'
   end
 end
